66b9ccb01ecc16f5bccd7c0202494b8246c23e0c
rjabeen04 <rjabeen122@gmail.com>
2026-01-30 11:13:25 -0600

    Create outputs.tf (#31)

---
+++ infra/terraform/modules/aks/outputs.tf
@@ -0,0 +1,7 @@
+output "name" { value = azurerm_kubernetes_cluster.this.name }
+output "id"   { value = azurerm_kubernetes_cluster.this.id }
+
+output "kube_config_raw" {
+  value     = azurerm_kubernetes_cluster.this.kube_config_raw
+  sensitive = true
+}
