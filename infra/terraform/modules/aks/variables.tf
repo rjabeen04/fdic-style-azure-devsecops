db78c36cda5beba888747ccbc0c0bf4600780581
rjabeen04 <rjabeen122@gmail.com>
2026-01-30 11:11:20 -0600

    Create variables.tf

---
+++ infra/terraform/modules/aks/variables.tf
@@ -0,0 +1,18 @@
+variable "name" { type = string }
+variable "location" { type = string }
+variable "resource_group_name" { type = string }
+variable "dns_prefix" { type = string }
+variable "subnet_id" { type = string }
+
+variable "log_analytics_workspace_id" { type = string }
+variable "acr_id" { type = string }
+
+variable "disk_encryption_set_id" {
+  type        = string
+  description = "Disk Encryption Set ID for AKS disk encryption"
+}
+
+variable "node_count" { type = number, default = 2 }
+variable "vm_size" { type = string, default = "Standard_DS2_v2" }
+
+variable "tags" { type = map(string), default = {} }
