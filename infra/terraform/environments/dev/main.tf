3c276b8e4a2475a1a57be137c5411dab14852268
rjabeen04 <rjabeen122@gmail.com>
2026-01-29 19:29:25 -0600

    Add comment for final test of Master CI pipeline (#17)

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -1,4 +1,5 @@
 # Phase 1 (dev): infrastructure resources will be added here.
 # Start with foundation modules (rg, vnet, log analytics, acr, aks, appgw+waf) in later steps.
 # test: clean ruleset enforcement
+# Final test of the Master CI pipeline
 
