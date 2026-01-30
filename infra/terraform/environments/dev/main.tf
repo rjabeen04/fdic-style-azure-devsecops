81d3c3f907b149c11e97fb11a963ef8ced1f11e6
rjabeen04 <rjabeen122@gmail.com>
2026-01-29 18:10:11 -0600

    Add comment for test rule enforcement (#12)
    Added a comment for test rule enforcement.

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -1,2 +1,4 @@
 # Phase 1 (dev): infrastructure resources will be added here.
 # Start with foundation modules (rg, vnet, log analytics, acr, aks, appgw+waf) in later steps.
+# test: clean ruleset enforcement
+
