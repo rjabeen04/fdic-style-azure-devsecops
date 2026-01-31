1a79b229480a316618512e4a5576420762e3f05d
rjabeen04 <rjabeen122@gmail.com>
2026-01-31 17:39:34 -0600

    Update main.tf

--- infra/terraform/modules/acr/main.tf
+++ infra/terraform/modules/acr/main.tf
@@ -3,8 +3,15 @@ resource "azurerm_container_registry" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
 
+  # ✅ Enterprise settings
   sku           = var.sku
   admin_enabled = var.admin_enabled
+
+  # ✅ CKV_AZURE_237 (Premium recommended)
+  data_endpoint_enabled = true
+
+  # ✅ CKV_AZURE_165 (geo-replication)
+  georeplications = var.georeplications
 
   tags = var.tags
 }
