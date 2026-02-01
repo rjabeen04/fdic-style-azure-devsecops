b2f3e055013ce633e4c040bb131a758579736d34
rjabeen04 <rjabeen122@gmail.com>
2026-01-31 19:51:38 -0600

    Create variables.tf

---
+++ infra/terraform/modules/network/variables.tf
@@ -0,0 +1,41 @@
+variable "name" {
+  description = "Virtual Network (VNet) name."
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group name where VNet and subnets will be created."
+  type        = string
+}
+
+variable "address_space" {
+  description = "VNet address space."
+  type        = list(string)
+}
+
+variable "subnets" {
+  description = <<EOT
+Map of subnets to create.
+Example:
+subnets = {
+  aks = { address_prefixes = ["10.10.1.0/24"], private_endpoint_policies_disabled = false }
+  private_endpoints = { address_prefixes = ["10.10.2.0/24"], private_endpoint_policies_disabled = true }
+}
+EOT
+
+  type = map(object({
+    address_prefixes                   = list(string)
+    private_endpoint_policies_disabled = optional(bool, false)
+  }))
+}
+
+variable "tags" {
+  description = "Tags applied to the VNet."
+  type        = map(string)
+  default     = {}
+}
