d6f9363e3c165f0e011150f039fe8731f72aed04
rjabeen04 <rjabeen122@gmail.com>
2026-01-31 18:44:39 -0600

    Update main.tf

--- infra/terraform/modules/acr/main.tf
+++ infra/terraform/modules/acr/main.tf
@@ -6,6 +6,12 @@ resource "azurerm_container_registry" "this" {
   sku           = "Premium"
   admin_enabled = false
 
+  # ✅ CKV_AZURE_139
+  public_network_access_enabled = false
+
+  # ✅ CKV_AZURE_233
+  zone_redundancy_enabled = true
+
   data_endpoint_enabled = true
 
   georeplications {
@@ -16,13 +22,18 @@ resource "azurerm_container_registry" "this" {
   # ✅ CKV_AZURE_167 - Cleanup untagged manifests
   retention_policy {
     enabled = true
-    days    = var.retention_days # e.g., 7 / 30
+    days    = var.retention_days
   }
 
   # ✅ CKV_AZURE_164 - Signed/trusted images (content trust)
   trust_policy {
     enabled = true
   }
+
+  # (Optional hardening)
+  # network_rule_set {
+  #   default_action = "Deny"
+  # }
 
   tags = var.tags
 }
