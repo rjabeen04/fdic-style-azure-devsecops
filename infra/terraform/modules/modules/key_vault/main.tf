1d46d8a60682415b13f0fc31d448afdf45aafc5d
rjabeen04 <rjabeen122@gmail.com>
2026-02-01 00:27:36 -0600

    Update main.tf

--- infra/terraform/modules/modules/key_vault/main.tf
+++ infra/terraform/modules/modules/key_vault/main.tf
@@ -1,5 +1,26 @@
 data "azurerm_client_config" "current" {}
 
+resource "azurerm_key_vault" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  sku_name = var.sku_name
+
+  purge_protection_enabled       = var.purge_protection_enabled
+  soft_delete_retention_days     = var.soft_delete_retention_days
+  enable_rbac_authorization      = true
+  public_network_access_enabled  = var.public_network_access_enabled
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
+
+  tags = var.tags
+}
+
 resource "azurerm_key_vault_key" "des" {
   name         = var.key_name
   key_vault_id = azurerm_key_vault.this.id
@@ -17,40 +38,27 @@ resource "azurerm_key_vault_key" "des" {
   ]
 }
 
-resource "azurerm_key_vault" "this" {
-  name                = var.name
+resource "azurerm_private_endpoint" "kv" {
+  count               = var.private_endpoint_enabled ? 1 : 0
+  name                = "${var.name}-pe"
   location            = var.location
   resource_group_name = var.resource_group_name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
+  subnet_id           = var.private_endpoint_subnet_id
 
-  sku_name = var.sku_name
-
-  # Enterprise hardening
-  purge_protection_enabled    = var.purge_protection_enabled
-  soft_delete_retention_days  = var.soft_delete_retention_days
-  enable_rbac_authorization   = true
-  public_network_access_enabled = var.public_network_access_enabled
-
-  # When public access is disabled, Private Endpoint is typical (added later).
-  # This ACL still expresses intent: deny by default.
-  network_acls {
-    default_action = "Deny"
-    bypass         = "AzureServices"
+  private_service_connection {
+    name                           = "${var.name}-pe-conn"
+    private_connection_resource_id = azurerm_key_vault.this.id
+    subresource_names              = ["vault"]
+    is_manual_connection           = false
   }
 
   tags = var.tags
 }
 
-resource "azurerm_key_vault_key" "des" {
-  name         = var.key_name
-  key_vault_id = azurerm_key_vault.this.id
-  key_type     = var.key_type
-  key_size     = var.key_size
+resource "azurerm_private_dns_zone_group" "kv" {
+  count               = var.private_endpoint_enabled && var.private_dns_zone_id != null ? 1 : 0
+  name                = "default"
+  private_endpoint_id = azurerm_private_endpoint.kv[0].id
 
-  key_opts = [
-    "encrypt",
-    "decrypt",
-    "wrapKey",
-    "unwrapKey",
-  ]
+  private_dns_zone_ids = [var.private_dns_zone_id]
 }
