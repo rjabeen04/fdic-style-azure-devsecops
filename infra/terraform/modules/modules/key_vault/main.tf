ebe72cf42ac0fa9b5d7685701e619ee96b5b8727
rjabeen04 <rjabeen122@gmail.com>
2026-02-01 00:17:45 -0600

    Create   main.tf

---
+++ infra/terraform/modules/modules/key_vault/main.tf
@@ -0,0 +1,39 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+
+  sku_name = var.sku_name
+
+  # Enterprise hardening
+  purge_protection_enabled    = var.purge_protection_enabled
+  soft_delete_retention_days  = var.soft_delete_retention_days
+  enable_rbac_authorization   = true
+  public_network_access_enabled = var.public_network_access_enabled
+
+  # When public access is disabled, Private Endpoint is typical (added later).
+  # This ACL still expresses intent: deny by default.
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_key_vault_key" "des" {
+  name         = var.key_name
+  key_vault_id = azurerm_key_vault.this.id
+  key_type     = var.key_type
+  key_size     = var.key_size
+
+  key_opts = [
+    "encrypt",
+    "decrypt",
+    "wrapKey",
+    "unwrapKey",
+  ]
+}
