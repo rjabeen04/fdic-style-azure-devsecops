d381cb63f7dcba525e6f47fb9e976a34db156c8e
rjabeen04 <rjabeen122@gmail.com>
2026-01-31 18:48:48 -0600

    Update main.tf

--- infra/terraform/modules/acr/main.tf
+++ infra/terraform/modules/acr/main.tf
@@ -19,6 +19,12 @@ resource "azurerm_container_registry" "this" {
     zone_redundancy_enabled = true
   }
 
+  # ✅ CKV_AZURE_166 - Image quarantine
+  quarantine_policy {
+  enabled = true
+ }
+
+
   # ✅ CKV_AZURE_167 - Cleanup untagged manifests
   retention_policy {
     enabled = true
