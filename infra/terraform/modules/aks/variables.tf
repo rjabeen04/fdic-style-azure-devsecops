3a02b75b2221cd654b672a0a917c5245ef67f29a
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 10:29:33 -0600

    Update variables.tf (#85)

--- infra/terraform/modules/aks/variables.tf
+++ infra/terraform/modules/aks/variables.tf
@@ -1,11 +1,30 @@
-variable "name" { type = string }
-variable "location" { type = string }
-variable "resource_group_name" { type = string }
-variable "dns_prefix" { type = string }
-variable "subnet_id" { type = string }
+variable "name" {
+  type = string
+}
 
-variable "log_analytics_workspace_id" { type = string }
-variable "acr_id" { type = string }
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "dns_prefix" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "log_analytics_workspace_id" {
+  type = string
+}
+
+variable "acr_id" {
+  type = string
+}
 
 variable "disk_encryption_set_id" {
   type        = string
@@ -17,7 +36,17 @@ variable "api_server_authorized_ip_ranges" {
   description = "Allowed IP ranges for AKS API server access"
 }
 
-variable "node_count" { type = number, default = 2 }
-variable "vm_size" { type = string, default = "Standard_DS2_v2" }
+variable "node_count" {
+  type    = number
+  default = 2
+}
 
-variable "tags" { type = map(string), default = {} }
+variable "vm_size" {
+  type    = string
+  default = "Standard_DS2_v2"
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
