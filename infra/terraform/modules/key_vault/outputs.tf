5b1997cc9cec66924c7f4429767ad588c4cf7d6b
rjabeen04 <rjabeen122@gmail.com>
2026-02-01 22:51:38 -0600

    Update outputs.tf

--- infra/terraform/modules/key_vault/outputs.tf
+++ infra/terraform/modules/key_vault/outputs.tf
@@ -1,3 +1,9 @@
 output "key_id" {
-  value = azurerm_key_vault_key.des.id
+  description = "Key Vault key ID for DES"
+  value       = azurerm_key_vault_key.des.id
+}
+
+output "id" {
+  description = "Key Vault ID"
+  value       = azurerm_key_vault.this.id
 }
