3922c7221d62e206066bdafe5d2a44019c2c30a4
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 11:31:23 -0600

    Create main.tf

---
+++ infra/terraform/modules/appgw_waf/main.tf
@@ -0,0 +1,82 @@
+resource "azurerm_public_ip" "pip" {
+  name                = "${var.name}-pip"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = var.tags
+}
+
+locals {
+  backend_address_pool_name      = "${var.name}-beap"
+  frontend_port_name             = "${var.name}-feport"
+  frontend_ip_configuration_name = "${var.name}-feip"
+  http_setting_name              = "${var.name}-be-htst"
+  listener_name                  = "${var.name}-httplstn"
+  request_routing_rule_name      = "${var.name}-rqrt"
+}
+
+resource "azurerm_application_gateway" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+
+  sku {
+    name     = var.sku_name
+    tier     = var.sku_tier
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = var.subnet_id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    priority                   = 1
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+  }
+
+  # ✅ FDIC Requirement: WAF Configuration
+  waf_configuration {
+    enabled          = true
+    firewall_mode    = "Prevention"
+    rule_set_type    = "OWASP"
+    rule_set_version = "3.2"
+  }
+
+  tags = var.tags
+}
