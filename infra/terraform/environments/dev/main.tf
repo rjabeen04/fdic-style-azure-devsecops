3d40969e0950a34dded1adcdf7e4f6aec9b2a5fe
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 10:01:04 -0600

    Update main.tf

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -69,53 +69,77 @@ module "log_analytics" {
 # Next modules (add later)
 ############################
 
-# 4) ACR (module later)
-# module "acr" {
-#   source              = "../../modules/acr"
-#   name                = "${local.prefix}-acr"
-#   location            = var.location
-#   resource_group_name = module.rg.name
-#
-#   # If your ACR module supports private endpoint wiring later:
-#   # private_endpoint_subnet_id = module.network.subnet_ids["private_endpoints"]
-#
-#   tags = local.tags
-# }
+############################
+# Step 4) ACR (Container Registry)
+############################
+module "acr" {
+  source              = "../../modules/acr"
+  name                = "${replace(local.prefix, "-", "")}acr"
+  location            = var.location
+  resource_group_name = module.rg.name
 
-# 5) Disk Encryption Set + Key Vault Key (module later)
-# module "des" {
-#   source              = "../../modules/des"
-#   name                = "${local.prefix}-des"
-#   location            = var.location
-#   resource_group_name = module.rg.name
-#   tags                = local.tags
-# }
+  tags = local.tags
+}
+############################
+# Step 5a) Key Vault
+############################
+module "key_vault" {
+  source              = "../../modules/key_vault"
+  name                = "${replace(local.prefix, "-", "")}kv"
+  location            = var.location
+  resource_group_name = module.rg.name
 
-# 6) AKS (module later)
-# NOTE: Keep commented until acr/des outputs exist.
-# module "aks" {
-#   source = "../../modules/aks"
-#
-#   name                = "${local.prefix}-aks"
-#   location            = var.location
-#   resource_group_name = module.rg.name
-#   dns_prefix          = local.prefix
-#
-#   subnet_id = module.network.subnet_ids["aks"]
-#
-#   log_analytics_workspace_id = module.log_analytics.workspace_id
-#   acr_id                     = module.acr.id
-#   disk_encryption_set_id      = module.des.id
-#
-#   api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
-#
-#   node_count      = 2
-#   vm_size         = "Standard_DS2_v2"
-#   user_node_count = 2
-#   user_vm_size    = "Standard_DS2_v2"
-#
-#   tags = local.tags
-# }
+  key_name        = "${replace(local.prefix, "-", "")}-des-key"
+  key_size        = 2048
+  key_expire_days = 365
+
+  # If your KV module supports private endpoint wiring later, you can add it:
+  # private_endpoint_enabled   = true
+  # private_endpoint_subnet_id = module.network.subnet_ids["private_endpoints"]
+
+  tags = local.tags
+}
+
+############################
+# Step 5b) Disk Encryption Set
+############################
+module "des" {
+  source              = "../../modules/des"
+  name                = "${local.prefix}-des"
+  location            = var.location
+  resource_group_name = module.rg.name
+
+  key_vault_key_id = module.key_vault.key_id
+
+  tags = local.tags
+}
+############################
+# Step 6) AKS
+############################
+module "aks" {
+  source = "../../modules/aks"
+
+  name                = "${local.prefix}-aks"
+  location            = var.location
+  resource_group_name = module.rg.name
+  dns_prefix          = local.prefix
+
+  subnet_id = module.network.subnet_ids["aks"]
+
+  log_analytics_workspace_id = module.log_analytics.workspace_id
+  acr_id                     = module.acr.id
+  disk_encryption_set_id      = module.des.id
+
+  api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
+
+  node_count      = 2
+  vm_size         = "Standard_DS2_v2"
+  user_node_count = 2
+  user_vm_size    = "Standard_DS2_v2"
+
+  tags = local.tags
+}
+
 
 # 7) AppGW + WAF (module later)
 # module "appgw_waf" { ... }
