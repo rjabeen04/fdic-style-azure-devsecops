89d880ee66aca3bf8fc5432df7c1f89445cb3cb4
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 19:50:55 -0600

    Create backend.tf (#97)

--- infra/terraform/environments/dev/backend.tf
+++ infra/terraform/environments/dev/backend.tf
@@ -4,7 +4,6 @@ terraform {
     storage_account_name = "sttfstate2419"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
-    use_oidc             = true
-    use_azuread_auth     = true
+
   }
 }
