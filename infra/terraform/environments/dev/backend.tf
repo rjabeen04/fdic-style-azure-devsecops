95fd817dec916fedd5b47b3d2b20e8513936c66e
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 18:51:57 -0600

    Create backend.tf (#99)
    * Create backend.tf
    
    * Update backend.tf
    
    * Update backend.tf
    
    * Update backend.tf

---
+++ infra/terraform/environments/dev/backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "sttfstate2419"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+    use_oidc             = true
+    use_azuread_auth     = true
+  }
+}
