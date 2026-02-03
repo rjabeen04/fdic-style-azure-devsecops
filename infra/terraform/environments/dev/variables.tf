220861eeb94701ad769aa2d3b6795dc7ff435094
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 23:27:24 -0600

    Update variables.tf

--- infra/terraform/environments/dev/variables.tf
+++ infra/terraform/environments/dev/variables.tf
@@ -9,3 +9,9 @@ variable "api_server_authorized_ip_ranges" {
   description = "Allowed IP ranges for AKS API server (dev). FDIC-style would be VPN/corp IPs."
   default     = ["0.0.0.0/0"]
 }
+variable "ssl_cert_password" {
+  type        = string
+  description = "Password for the App Gateway SSL certificate"
+  sensitive   = true
+  default     = "P@ssw0rd1234!" 
+}
