b563627bbe9b49d1791cbe73b7ca25bd050c1a3f
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 21:48:15 -0600

    Update main.tf

--- infra/terraform/environments/dev/main.tf
+++ infra/terraform/environments/dev/main.tf
@@ -66,56 +66,83 @@ module "log_analytics" {
 }
 
 ############################
-# Next modules (add later)
+# Phase 2: Security & Storage 
 ############################
 
-# 4) ACR (module later)
-# module "acr" {
-#   source              = "../../modules/acr"
-#   name                = "${local.prefix}-acr"
-#   location            = var.location
-#   resource_group_name = module.rg.name
-#
-#   # If your ACR module supports private endpoint wiring later:
-#   # private_endpoint_subnet_id = module.network.subnet_ids["private_endpoints"]
-#
-#   tags = local.tags
-# }
+# 4) ACR (For your musical-volunteer images)
+module "acr" {
+  source              = "../../modules/acr"
+  # Using a unique name to avoid "Already Exists" errors
+  name                = "acrfdicdev0202" 
+  location            = var.location
+  resource_group_name = module.rg.name
+  tags                = local.tags
+}
 
-# 5) Disk Encryption Set + Key Vault Key (module later)
-# module "des" {
-#   source              = "../../modules/des"
-#   name                = "${local.prefix}-des"
-#   location            = var.location
-#   resource_group_name = module.rg.name
-#   tags                = local.tags
-# }
+# 5) Key Vault (Required for Encryption)
+module "key_vault" {
+  source              = "../../modules/key_vault"
+  name                = "${local.prefix}-kv"
+  location            = var.location
+  resource_group_name = module.rg.name
+  tags                = local.tags
+}
 
-# 6) AKS (module later)
-# NOTE: Keep commented until acr/des outputs exist.
-# module "aks" {
-#   source = "../../modules/aks"
-#
-#   name                = "${local.prefix}-aks"
-#   location            = var.location
-#   resource_group_name = module.rg.name
-#   dns_prefix          = local.prefix
-#
-#   subnet_id = module.network.subnet_ids["aks"]
-#
-#   log_analytics_workspace_id = module.log_analytics.workspace_id
-#   acr_id                     = module.acr.id
-#   disk_encryption_set_id      = module.des.id
-#
-#   api_server_authorized_ip_ranges = var.api_server_authorized_ip_ranges
-#
-#   node_count      = 2
-#   vm_size         = "Standard_DS2_v2"
-#   user_node_count = 2
-#   user_vm_size    = "Standard_DS2_v2"
-#
-#   tags = local.tags
-# }
+# 6) Disk Encryption Set
+module "des" {
+  source              = "../../modules/des"
+  name                = "${local.prefix}-des"
+  location            = var.location
+  resource_group_name = module.rg.name
+  key_vault_key_id    = module.key_vault.key_id 
+  tags                = local.tags
+}
 
-# 7) AppGW + WAF (module later)
-# module "appgw_waf" { ... }
+############################
+# Phase 3: Traffic & Compute
+############################
+
+# 7) AppGW + WAF (Using the variables we just fixed!)
+module "appgw_waf" {
+  source                    = "../../modules/appgw_waf"
+  name                      = "${local.prefix}-appgw"
+  resource_group_name       = module.rg.name
+  location                  = var.location
+  subnet_id                 = module.network.subnet_ids["appgw"] # Make sure "appgw" is in your subnets list!
+  
+  backend_fqdn              = "musical-volunteer.local"
+  backend_root_cert_data    = "" 
+  frontend_cert_pfx_base64  = ""
+  frontend_cert_password    = "DummyPassword123!"
+  ssl_cert_password         = "DummyPassword123!"
+
+  tags                      = local.tags
+}
+
+# 8) AKS (The Cluster)
+# Note: This will take ~10-15 minutes to deploy
+module "aks" {
+  source = "../../modules/aks"
+
+  name                = "${local.prefix}-aks"
+  location            = var.location
+  resource_group_name = module.rg.name
+  dns_prefix          = local.prefix
+
+  subnet_id = module.network.subnet_ids["aks"]
+
+  log_analytics_workspace_id = module.log_analytics.workspace_id
+  acr_id                     = module.acr.id
+  disk_encryption_set_id      = module.des.id
+
+  node_count      = 2
+  vm_size         = "Standard_DS2_v2"
+  user_node_count = 2
+  user_vm_size    = "Standard_DS2_v2"
+
+  tags = local.tags
+}
+
+
+
+
