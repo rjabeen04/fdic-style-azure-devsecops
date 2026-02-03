5657b5c880fca39761453eb24805b7f5ba9881a2
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 18:23:27 -0600

    Update backend.tf

--- infra/terraform/environments/dev/backend.tf
+++ infra/terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "fdic-dev-rg"
+    resource_group_name  = "rg-tfstate"
     storage_account_name = "sttfstate2419"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
