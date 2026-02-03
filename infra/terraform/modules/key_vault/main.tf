d512241ba4375dd6a8d0e51259b99573338efbd9
rjabeen04 <rjabeen122@gmail.com>
2026-02-02 23:04:37 -0600

    Update main.tf (#115)
    * Update main.tf
    
    * Update main.tf

--- infra/terraform/modules/key_vault/main.tf
+++ infra/terraform/modules/key_vault/main.tf
@@ -1,15 +1,15 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "this" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-
-  sku_name = var.sku_name
-
-  purge_protection_enabled      = var.purge_protection_enabled
-  soft_delete_retention_days    = var.soft_delete_retention_days
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  sku_name                      = "standard"
+  
+  # ✅ Security best practices for Checkov
+  purge_protection_enabled      = true
+  soft_delete_retention_days    = 7
   enable_rbac_authorization     = true
   public_network_access_enabled = false
 
@@ -20,13 +20,15 @@ resource "azurerm_key_vault" "this" {
 
   tags = var.tags
 }
+
 resource "azurerm_key_vault_key" "des" {
+  # checkov:skip=CKV_AZURE_112: HSM is not available in Standard SKU. Using software-backed RSA for cost control.
+  # checkov:skip=CKV_AZURE_40: Expiration date is dynamically calculated via timeadd.
   name         = var.key_name
   key_vault_id = azurerm_key_vault.this.id
-  key_type     = "RSA-HSM"
-  key_size     = var.key_size
+  key_type     = "RSA"
+  key_size     = 2048
 
-  # ✅ Fixes CKV_AZURE_40
   expiration_date = timeadd(timestamp(), "${var.key_expire_days * 24}h")
 
   key_opts = [
@@ -36,6 +38,7 @@ resource "azurerm_key_vault_key" "des" {
     "unwrapKey",
   ]
 }
+
 resource "azurerm_private_endpoint" "kv" {
   count               = var.private_endpoint_enabled ? 1 : 0
   name                = "${var.name}-pe"
@@ -50,7 +53,6 @@ resource "azurerm_private_endpoint" "kv" {
     is_manual_connection           = false
   }
 
-  # This replaces the two broken standalone resources
   dynamic "private_dns_zone_group" {
     for_each = var.private_dns_zone_id != null ? [1] : []
     content {
